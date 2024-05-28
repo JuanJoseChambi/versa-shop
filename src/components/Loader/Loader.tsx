f2f5baac9a92a6508ebf657f6535c5d8f166a5e0
JoseChambi <chambijuanjose05@gmail.com>
2024-05-27 22:49:52 -0300

    [CHANGE] Loader/ style

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,23 +1,21 @@
 
 interface LoaderProp {
     active:boolean | undefined;
-    label?:string;
 }
 
-function Loader({active, label}:LoaderProp) {
+function Loader({active}:LoaderProp) {
 
     if (!active) return
 
   return (
-    <main className="w-full h-screen fixed top-0 left-0 flex justify-center items-center flex-col bg-white z-[110]">
+    <main className="w-full min-h-screen max-h-screen fixed top-0 left-0 flex justify-center items-center flex-col bg-white z-[110]">
       <section className="my-auto flex justify-center items-center flex-col gap-y-4">
-        <i className="bx bx-loader-alt bx-spin text-3xl "></i>
-        <h3 className="text-2xl text-neutral-700 font-semibold tracking-widest">LOADING</h3>
+        <i className="bx bx-loader-alt bx-spin text-3xl text-neutral-800"></i>
+        <h3 className="text-sm text-neutral-700 font-semibold tracking-widest animate-pulse">LOADING</h3>
       </section>
-      <section className="absolute bottom-5 flex justify-center items-center flex-col">
-        <h3 className="text-3xl font-noto">VERSA</h3>
-        <h4 className="text-sm font-noto text-neytral-700">{label}</h4>
-      </section>
+      <div className="w-[90%] h-[91vh] absolute border border-neutral-800 rounded-sm"></div>
+      <h3 className="absolute bottom-5 text-3xl font-noto font-light px-3 bg-white">VERSA</h3>
+    
     </main>
   )
 }
