f50e2821e15aa37e49e674472ebc895be1495948
JoseChambi <chambijuanjose05@gmail.com>
2024-06-10 00:01:51 -0300

    [Test] Shop-CardProduct/Select Size-Color

--- src/components/CardProduct/CardProduct.tsx
+++ src/components/CardProduct/CardProduct.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { DataProduct } from "../../interfaces/interfaces"
 import { Link } from "react-router-dom"
 
@@ -6,7 +7,16 @@ interface CardProductProp {
 }
 
 function CardProduct({product}:CardProductProp) {
+    const [size, setSize] = useState<string | null>(null)
+    const [hoverImage, setHoverImage] = useState(false)
 
+    const color = product.Stocks.filter(sizeStock => sizeStock.Size.size === size)
+    // console.log(color);
+    
+    const sizeUnique = new Set(product.Stocks.map(stock => stock.Size.size));
+    const sizes = [...sizeUnique]
+    // console.log(sizes);
+    
     const colorsAvailable = new Set(product.Stocks.map(stock => stock.Color.color));
     const hxacolorAvailable = new Set(product.Stocks.map(stock => stock.Color.hxacolor));
     const uniqueColorsQuantity = [...colorsAvailable]
@@ -14,11 +24,27 @@ function CardProduct({product}:CardProductProp) {
     
   return (
     <article key={product.product_id} className="max-w-[276px] min-h-[320px] relative flex justify-start items-start flex-col bg-redd-500 p-3 outline-none hover:outline-1 hover:outline hover:outline-neutral-300 transition-[outline] duration-500 ">
-                <Link to={`/detail/${product.product_id}`}>
-                    <picture className="w-[250px] min-h-[220px] max-h-[220px] flex justify-center items-center overflow-hidden bg-blued-500">
-                        <img src={product.image} alt={product.name} className="w-[80%]"/>
+                {/* <Link to={`/detail/${product.product_id}`}> */}
+                    <picture className="w-[250px] min-h-[220px] max-h-[220px] relative flex justify-center items-center overflow-hidden bg-blued-500" onMouseEnter={() => setHoverImage(true)}
+                        onMouseDown={() => setHoverImage(true)} onMouseLeave={() => {setHoverImage(false), setSize(null)}}>
+                        <img src={product.image} alt={product.name} className="w-[90%] h-full object-cover"/>
+                        
+                        <div className={`${hoverImage ? "w-full h-full bg-[#0000007c] absolute top-0 left-0 flex justify-center items-center rounded-sm": "hidden"}`}>
+                            <div className="w-full flex justify-center items-center gap-3">
+                                {color.map(col => (
+                                    <div className={`w-[25px] h-[25px] rounded-full cursor-pointer`} style={{backgroundColor: col.Color.hxacolor}}></div>
+                                ))}
+                            </div>
+                            <div className="bg-redd-500 text-white absolute bottom-3 flex justify-center items-center gap-x-3">
+                                {sizes.map(sizeUnique => {
+                                    
+                                return(
+                                    <p className={`${size === sizeUnique ? "bg-black text-white " : "bg-white text-black"} transition-all duration-500 px-2 py-1 cursor-pointer`} onClick={() => setSize(sizeUnique)}>{sizeUnique}</p>
+                                )})}
+                            </div>
+                        </div>
                     </picture>
-                </Link>
+                {/* </Link> */}
                 <Link to={`/detail/${product.product_id}`} className="w-full min-h-[80px] max-h-[80px] flex flex-col justify-between items-start bg-greend-500 ">
                     <h5 className="font-semibold text-lg text-neutral-800 tracking-widest leading-5 text-clipping">{product.name}</h5>
                     <div className="w-full flex justify-between items-center mt-auto ">
@@ -26,9 +52,6 @@ function CardProduct({product}:CardProductProp) {
                         <div className="flex justify-center items-center flex-col gap-y-1 ">
                             <p className="text-sm text-neutral-500">{uniqueColorsQuantity.length} Colores</p>
                             <div className="flex justify-center items-center gap-x-1 ">
-                                {/* {product.Stocks.map(stock => (
-                                    <button key={`${product.product_id}${stock.stock_id}`} style={{backgroundColor:stock?.Color?.hxacolor}} className={`flex justify-center items-center min-w-[15px] min-h-[15px] rounded-full`}></button>
-                                    ))} */}
                                 {uniqueHxacolorQuantity.map(color => (
                                     <button key={`${product.product_id}`} style={{backgroundColor:color}} className={`flex justify-center items-center min-w-[15px] min-h-[15px] rounded-full`}></button>
                                 ))}
