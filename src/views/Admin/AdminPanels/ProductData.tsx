34b4f15b0c8ca7a536456a46037e578c7eb68952
JoseChambi <chambijuanjose05@gmail.com>
2024-06-12 22:35:00 -0300

    [ADD] AdminPanel-ProductData/Create-Delete

--- src/views/Admin/AdminPanels/ProductData.tsx
+++ src/views/Admin/AdminPanels/ProductData.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 // import { useFilter } from "../../../hooks/useFilter"
 import { fetchPOST } from "../../../utils/fetchPOST";
 import { allFilters } from "../../../utils/allFilters";
+import { fetchDELETE } from "../../../utils/fetchDELETE";
 const {VITE_URL_BASE} = import.meta.env
 
 interface DataProp {
@@ -10,20 +11,23 @@ interface DataProp {
     state: any;
     filter: string[] | {color:string, hxacolor:string}[];
     change?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    onClickAdd: () => void
+    onClickAdd?: () => void;
+    onClickDelete?:(data:string, param:string) => void
 }
 
-function Data ({filter, title, data, state, change, onClickAdd}: DataProp) {
+function Data ({filter, title, data, state, change, onClickAdd, onClickDelete}: DataProp) {
     const [open, setOpen] = useState<string | null>(null)
     const [add, setAdd] = useState<string | null>(null)
+    const [deleted, setDeleted] = useState<string | null>(null)
 
     return (
         <section className="w-full px-3 ">
             <div className="w-full cursor-pointer flex justify-between items-center" >                   
-                <h3 className="w-full py-5 text-lg text-neutral-800 font-semibold bg-redd-500" onClick={() => {open && open === data ? (setAdd(null), setOpen(null)) : setOpen(data)}}>{title}</h3>
+                <h3 className="w-full py-5 text-lg text-neutral-800 font-semibold bg-redd-500" onClick={() => {open && open === data ? (setAdd(null), setOpen(null), setDeleted(null)) : setOpen(data)}}>{title}</h3>
                 <div className="flex justify-center items-center gap-x-3">
                     {open && open === data && <i className="bx bx-plus scale-125" onClick={() => {add ? setAdd(null) : setAdd(data)}}/>}
                     {open && open === data && <i className="bx bx-edit scale-110"/>}
+                    {open && open === data && <i className="bx bx-trash scale-110" onClick={() => deleted ? setDeleted(null) : setDeleted(data)}/>}
                     
                     <i className={`${open === data ? "bx bx-chevron-up" : "bx bx-chevron-down"} scale-150`}/>
                 </div>
@@ -39,9 +43,10 @@ function Data ({filter, title, data, state, change, onClickAdd}: DataProp) {
                 </div>}
                 {filter?.map((value, index) => (
                     <React.Fragment key={index}>
-                        <li className="flex gap-x-2">
+                        <li className="flex justify-center items-center gap-x-2">
                             {typeof value === "object" && value?.hxacolor && <span className="w-[20px] h-[20px] rounded-full border border-neutral-600" style={{backgroundColor:value.hxacolor}}></span>}
                             {typeof value === "string" ? value : value?.color}
+                            {deleted && <i className="bx bx-trash scale-110 cursor-pointer" onClick={() => onClickDelete && onClickDelete(data, value.toString())}/>}
                         </li>
                     </React.Fragment>    
                 ))}
@@ -52,10 +57,10 @@ function Data ({filter, title, data, state, change, onClickAdd}: DataProp) {
 
 
 interface DataSend {
-    category:undefined | string;
-    type:undefined | string;
-    color:undefined | string;
-    size:undefined | string;
+    category?:undefined | string;
+    type?:undefined | string;
+    color?:undefined | string;
+    size?:undefined | string;
 }
 function ProductData() {
     const [dataSend, setDataSend] = useState<DataSend>({
@@ -65,34 +70,15 @@ function ProductData() {
         size:undefined
     })
 
-    async function hanlderCategory () {
-        // e.preventDefault()
-        const {data} = await fetchPOST(`${VITE_URL_BASE}/category/create`, {category:dataSend.category})
-        console.log(data);
-        
-    }
-    async function hanlderType () {
-            // e.preventDefault()
-            const {data} = await fetchPOST(`${VITE_URL_BASE}/type/create`, {type:dataSend.type})
-            console.log(data);
-            
-    }
-    async function hanlderColor () {
-        // e.preventDefault()
-        const {data} = await fetchPOST(`${VITE_URL_BASE}/color/create`, {color:dataSend.color})
-        console.log(data);
-        
-    }
-    async function hanlderSize () {
-        // e.preventDefault()
-        const {data} = await fetchPOST(`${VITE_URL_BASE}/size/create`, {size:dataSend.size})
-        console.log(data);
-        
-    }   
     const {categories, colors, sizes, types} = allFilters()
 
+    async function createData (data:string, body:DataSend) {
+        await fetchPOST(`${VITE_URL_BASE}/${data}/create`, body)
+    }
 
-    
+    async function deleteData (data:string, param:string) {
+        await fetchDELETE(`${VITE_URL_BASE}/${data}/delete/${param}`)
+    }
 
   return (
     <section className="w-[95%] flex justify-center items-center flex-col flex-wrap bg-redd-500 gap-3">
@@ -107,28 +93,34 @@ function ProductData() {
                 filter={categories} 
                 state={dataSend.category} 
                 change={(e) => setDataSend({...dataSend, category:e.target.value})} 
-                onClickAdd={hanlderCategory}/>
+                onClickAdd={() => createData("category", {category:dataSend.category})}
+                onClickDelete={deleteData}
+                />
             <Data 
                 title="TIPOS" 
                 data="type" 
                 filter={types} 
                 state={dataSend.type} 
                 change={(e) => setDataSend({...dataSend, type:e.target.value})} 
-                onClickAdd={hanlderType}/>
+                onClickAdd={() => createData("type", {type:dataSend.type})}
+                onClickDelete={deleteData}
+                />
             <Data 
                 title="COLORES" 
                 data="color" 
                 filter={colors} 
                 state={dataSend.color} 
                 change={(e) => setDataSend({...dataSend, color:e.target.value})} 
-                onClickAdd={hanlderColor}/>
+                onClickAdd={() => createData("color", {color:dataSend.color})}/>
             <Data 
                 title="TALLES"
                 data="size" 
                 filter={sizes} 
                 state={dataSend.size} 
                 change={(e) => setDataSend({...dataSend, size:e.target.value})} 
-                onClickAdd={hanlderSize}/>
+                onClickAdd={() => createData("size", {size:dataSend.size})}
+                onClickDelete={deleteData}
+                />
         </section>
     </section>
   )
