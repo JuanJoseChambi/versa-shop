66faad6d38f0d0e6939bb4350c141ea7a54d654e
JoseChambi <chambijuanjose05@gmail.com>
2024-07-03 22:00:00 -0300

    [ADD] Shop/ Button Redirect Whatsapp Api

--- src/views/Shop/Shop.tsx
+++ src/views/Shop/Shop.tsx
@@ -14,6 +14,9 @@ function Shop() {
       <HeaderShop/>
       <MainShop/>
       <Footer/>
+      <a href="https://wa.me/+5491161000622?text=Hola%2C%20estoy%20interesado." target="_blank" className="w-[50px] h-[50px] 2xl:w-[70px] 2xl:h-[70px] cursor-pointer rounded-full bg-green-500 fixed bottom-4 right-5 flex justify-center items-center">
+        <i className="bx bxl-whatsapp text-[50px] 2xl:text-7xl text-white"/>
+      </a>
     </>
   )
 }
