96b6d881ee9722faf85647ec5204585a33d0db44
JoseChambi <chambijuanjose05@gmail.com>
2024-07-26 23:15:26 -0300

    [BUG] CheckoutPayment/PyamentDebitCredit

--- src/components/PaymentDebitCredit/PaymentDebitCredit.tsx
+++ src/components/PaymentDebitCredit/PaymentDebitCredit.tsx
@@ -1,18 +1,24 @@
 import { Payment } from '@mercadopago/sdk-react';
-// import { initMercadoPago } from "@mercadopago/sdk-react"
-// import { useEffect } from 'react';
+import { error as errorAlert, normal } from '../../utils/alert';
 const { VITE_URL_BASE } = import.meta.env
 
 declare global {
     interface Window {
       MercadoPago: any;
     }
-  }
+}
 
 interface PaymentBrickProp {
 preferenceId: string;
 }
 
+
+function generateUUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+  });
+}
 
 function PaymentDebitCredit  ({preferenceId}: PaymentBrickProp) {
 
@@ -36,19 +42,24 @@ function PaymentDebitCredit  ({preferenceId}: PaymentBrickProp) {
   }
 
 
-  const onSubmit = async (
-    { selectedPaymentMethod, formData } : {selectedPaymentMethod:any , formData:any}
-  ) => {
-    console.log(selectedPaymentMethod);
+  async function onSubmit ( { selectedPaymentMethod, formData } : {selectedPaymentMethod:any , formData:any} ) {
     
+    console.log(selectedPaymentMethod);
     // callback llamado al hacer clic en el botón enviar datos
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve: any, reject: any) => {
+      if (selectedPaymentMethod === 'wallet_purchase' || selectedPaymentMethod === 'onboarding_credits') {
+        resolve();
+      }
+      console.log(generateUUID());
+      
+      formData.description = "Debito o credito";
       fetch(`${VITE_URL_BASE}/payment/process_payment`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
+          "X-Idempotency-Key": generateUUID()
         },
-        body: JSON.stringify(formData),
+        body: JSON.stringify({selectedPaymentMethod, formData}),
       })
         .then((response) => response.json())
         .then((response: any) => {
@@ -60,17 +71,19 @@ function PaymentDebitCredit  ({preferenceId}: PaymentBrickProp) {
           reject(error);
         });
     });
-   };
-   const onError = async (error: any) => {
+  };
+  const onError = async (error: any) => {
     // callback llamado para todos los casos de error de Brick
     console.log(error);
-   };
-   const onReady = async () => {
-    /*
+    errorAlert(error)
+  };
+  const onReady = async () => {
+      /*
       Callback llamado cuando el Brick está listo.
       Aquí puede ocultar cargamentos de su sitio, por ejemplo.
-    */
-   };
+     */
+    normal("Brick Listo!")
+  };
 
   return <div className='w-full'>
     <Payment
