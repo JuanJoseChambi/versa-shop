0dee959d7bc14aef24c9a0fdbdd0da119bdaac1d
JoseChambi <chambijuanjose05@gmail.com>
2024-07-30 17:02:44 -0300

    [BUG] Terface MP

--- src/components/PaymentDebitCredit/PaymentDebitCredit.tsx
+++ src/components/PaymentDebitCredit/PaymentDebitCredit.tsx
@@ -29,12 +29,12 @@ function PaymentDebitCredit  ({preferenceId}: PaymentBrickProp) {
 
   
   const customization = {
-    visual: {
-      hideFormTitle: true,
-    },
+    // visual: {
+      // hideFormTitle: true,
+    // },
     paymentMethods: {
-      creditCard: "all" as "all",
-      debitCard: "all" as "all",
+      creditCard: "all" as "all" ,
+      debitCard: "all" as "all" ,
       bankTransfer: "all",
       atm: "all",
       maxInstallments: 1
@@ -98,116 +98,3 @@ function PaymentDebitCredit  ({preferenceId}: PaymentBrickProp) {
 };
 
 export default PaymentDebitCredit;
-
-
-
-
-
-
-// import { useEffect, useRef } from 'react';
-// const {VITE_MP_P_KEY_BRICKS } = import.meta.env
-
-// declare global {
-//     interface Window {
-//       MercadoPago: any;
-//     }
-//   }
-
-// interface PaymentBrickProp {
-// preferenceId: string;
-// }
-
-// function PaymentDebitCredit  ({preferenceId}: PaymentBrickProp) {
-//   const paymentBrickContainerRef = useRef<HTMLDivElement | null>(null);
-
-//   useEffect(() => {
-//     const loadScript = (src: string) => {
-//       return new Promise((resolve, reject) => {
-//         const script = document.createElement('script');
-//         script.src = src;
-//         script.async = true;
-//         script.onload = resolve;
-//         script.onerror = reject;
-//         document.body.appendChild(script);
-//       });
-//     };
-
-//     const initializePaymentBrick = async () => {
-//       try {
-//         await loadScript('https://sdk.mercadopago.com/js/v2');
-//         const mp = new window.MercadoPago(VITE_MP_P_KEY_BRICKS, {
-//           locale: 'es'
-//         });
-//         const bricksBuilder = mp.bricks();
-
-//         const settings = {
-//           initialization: {
-//             amount: 10000,
-//             preferenceId: preferenceId,
-//             payer: {
-//                 firstName: "",
-//                 lastName: "",
-//                 email: "",
-//             },
-//             },
-//             customization: {
-//                 visual: {
-//                     hideFormTitle: true,
-//                     style: {
-//                     theme: "default",
-//                     },
-//                 },
-//                 paymentMethods: {
-//                     creditCard: "all",
-// 		                debitCard: "all",
-// 		                bankTransfer: "all",
-// 		                atm: "all",
-//                     maxInstallments: 1  
-//                 },
-//             },
-//             callbacks: {
-//             onReady: () => {
-//                 console.log('Payment brick is ready');
-//             },
-//             onSubmit: ({ selectedPaymentMethod, formData }: { selectedPaymentMethod: any, formData: any }) => {
-//                 console.log(selectedPaymentMethod);
-//                 console.log(formData);
-                
-//                 return new Promise((resolve, reject) => {
-//                   fetch('/process_payment', {
-//                       method: 'POST',
-//                       headers: {
-//                           'Content-Type': 'application/json',
-//                       },
-//                       body: JSON.stringify(formData),
-//                   })
-//                     .then((response) => response.json())
-//                     .then((response) => {
-//                         resolve(response);
-//                     })
-//                     .catch((error) => {
-//                         reject(error);
-//                     });
-//                 });
-//             },
-//             onError: (error: any) => {
-//                 console.error('Payment brick error:', error);
-//             },
-//             },
-//         };
-
-//         if (paymentBrickContainerRef.current) {
-//             await bricksBuilder.create('payment', 'paymentBrick_container', settings);
-//         }
-//         } catch (error) {
-//         console.error('Error loading MercadoPago script:', error);
-//         }
-//     };
-
-//     initializePaymentBrick();
-//   }, []);
-
-//   return <div className='w-full' id="paymentBrick_container" ref={paymentBrickContainerRef}></div>;
-// };
-
-// export default PaymentDebitCredit;
